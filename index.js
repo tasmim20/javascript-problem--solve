f04ea6c0363aa6438c211b08e493d1f9c38d7542
Tasmim Rahman <tasmimrahman2000@gmail.com>
2023-01-04 18:32:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+//1.Print all even numbers from 0 – 10
+// var i;
+// for(i = 0; i<=10; i=i+2){
+//     console.log(i);
+// }
+
+
+// 2.Print a table containing multiplication tables
+// let n = 5;
+// for(let i = 1; i<= 10; ++i){
+//     const table = n + "*" + i + "=" + n*i;
+//     console.log(table)
+// }
+
+
+//4. Calculate the sum of numbers within an array
+// let numbers = [23, 22, 43, 12, 32, 15];
+// let sum = 0;
+// for(let i = 0; i < numbers.length; i++){
+//     let number = numbers[i];
+//      sum = sum + number;   
+// }
+// console.log(sum);
+
+
+//5.Create a function that reverses an array
+// function reverseArray(number){
+//     for(let i = number.length-1; i >= 0; i--){    
+//     const element = number[i];
+//     console.log(element);
+// }
+// }
+// const number = [12, 45, 45, 32, 93];
+// const reversed = reverseArray(number);
+
+
+// 6. Sort an array from lowest to highest
+// var numberArray = new Float64Array([100, 104, 99]);
+// numberArray = numberArray.sort();
+// console.log(numberArray)
+
+
+//7.Create a function that filters out negative numbers
+// const numbers = [32, 33, 16, 40, -34, -12, 25];
+// const positiveNum = numbers.filter(checkPositive);
+// function checkPositive(number) {
+// return number > 0;
+// }
+// console.log(positiveNum)
+
+
+//8.Remove the spaces found in a string
+// const string ='  Tasmim Rahman   ';
+// console.log(string.split(' ').join(''));
+
+
+//9.Return a Boolean if a number is divisible by 10
+// const booleanNumber = (num) => {
+//     return num % 10 === 0 ? true : false
+// }
+// console.log(booleanNumber(50)) 
+
+
+//10.Return the numbers of vowels in a given string
+// var string = "I am a lazy girl"
+// //function to count number of vowels in a string
+// function countVowels(string) {
+//   var count = string.match(/[aeiou]/gi);
+//   return count === null ? 0 : count.length;
+// }
+// console.log(countVowels(string))
+
+
+
+
+
+
+
+
+
